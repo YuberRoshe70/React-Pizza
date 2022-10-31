bac305b92fc9bda950e769cfc2166a61b8ded4ec
Madosse <madoss@yandex.ru>
2022-10-31 16:55:25 +0300

    hhh

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
                 <Link to='/'>    <div className="header__logo">
                     <img width="38" src="./img/pizza-logo.svg" alt="Pizza logo" />
                     <div>
-                        <h1>React Pizza V2.0</h1>
+                        <h1>React Pizza V2</h1>
                         <p>самая вкусная пицца во вселенной</p>
                     </div>
                 </div></Link>
